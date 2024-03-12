f4fa419fd9daa6df57800e0385742a2f1e65458b
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-03-12 20:00:45 +0300

    shape main file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Conversations from "./components/Conversations";
 import Navbar from "./components/navBar";
+import conversations from "./data/conversations";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <div className="body">
+        <Conversations conversations={conversations} />
+      </div>
     </div>
   );
 }
