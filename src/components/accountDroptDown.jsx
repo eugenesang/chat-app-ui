5240d3b6aee33995e2663a4eef8dbc91f6b787ae
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-03-13 19:20:33 +0300

    message options ui

--- src/components/accountDroptDown.jsx
+++ src/components/accountDroptDown.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const AccountDropdown = ({
     email,
@@ -6,53 +6,64 @@ const AccountDropdown = ({
     profileImage,
     close,
 }) => {
+    const childRef = useRef(null);
+
+    const handleParentClick = (event) => {
+        // Check if the click originated from the child element using the ref
+        if (!childRef.current || !childRef.current.contains(event.target)) {
+            close();
+        }
+    };
+
     return (
-        <section id="account-drop-down">
-            <div className="acc-container">
-                <div className="header">
-                    <div className="left">
-                        <span>{email}</span>
+        <div className="dropdown-container" onClick={handleParentClick} >
+            <section id="account-drop-down" ref={childRef}>
+                <div className="acc-container">
+                    <div className="header">
+                        <div className="left">
+                            <span>{email}</span>
+                        </div>
+                        <div className="right">
+                            <button onClick={close}>
+                                <i className="fas fa-xmark"></i>
+                            </button>
+                        </div>
                     </div>
-                    <div className="right">
-                        <button onClick={close}>
-                            <i className="fas fa-xmark"></i>
+                    <div className="content">
+                        <div className="top">
+                            <img src={profileImage} alt={`${name}`} />
+                        </div>
+                        <div className="greetings">
+                            <span>Hi, {name}!</span>
+                        </div>
+                        <div className="action-btn">
+                            <button>Manage your Account</button>
+                        </div>
+                    </div>
+                    <div className="actions-container">
+
+                        <button className='left'>
+                            <i className="fas fa-sign-out-alt"></i>
+                            <span>Logout</span>
                         </button>
-                    </div>
-                </div>
-                <div className="content">
-                    <div className="top">
-                        <img src={profileImage} alt={`${name}`} />
-                    </div>
-                    <div className="greetings">
-                        <span>Hi, {name}!</span>
-                    </div>
-                    <div className="action-btn">
-                        <button>Manage your Account</button>
-                    </div>
-                </div>
-                <div className="actions-container">
+                        <button className='right'>
+                            <i className="fas fa-cog"></i>
+                            <span>Settings</span>
+                        </button>
 
-                    <button className='left'>
-                        <i className="fas fa-sign-out-alt"></i>
-                        <span>Logout</span>
-                    </button>
-                    <button className='right'>
-                        <i className="fas fa-cog"></i>
-                        <span>Settings</span>
-                    </button>
-
+                    </div>
+                    <div className="foot">
+                        <span className='link'>
+                            <span>By</span> <a href="https://github.com/eugenesang" target="_blank" rel="noreferrer" >Eugene Sang</a>
+                        </span>
+                        <span className='dot'>·</span>
+                        <span>
+                            More like this
+                        </span>
+                    </div>
                 </div>
-                <div className="foot">
-                    <span className='link'>
-                        <span>By</span> <a href="https://github.com/eugenesang" target="_blank" rel="noreferrer" >Eugene Sang</a>
-                    </span>
-                    <span className='dot'>·</span>
-                    <span>
-                        More like this
-                    </span>
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     );
 }
 
